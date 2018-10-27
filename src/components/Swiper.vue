7be6d2aecabf8ec43139cfbaa445200be91a878a
FENGXIUWEI <1428915751@qq.com>
2018-10-27 21:28:19 +0800

    home-swiper

---
+++ src/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="item of swiperlist" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>/* 需要引入此插件npm install vue-awesome-swiper --save */
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  name: 'Swiper',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperlist: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+      },
+      {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+      },
+      {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .swiper
+    overflow:hidden
+    height:0
+    padding-bottom:31.25%
+    width: 100%
+    background:#eee
+    .swiper-img
+      width:100%
+</style>
