030761b77506c17a6906a22038fb1a1b02f3403d
FENGXIUWEI <1428915751@qq.com>
2018-10-28 15:50:09 +0800

    search

--- src/page/city/City.vue
+++ src/page/city/City.vue
@@ -1,13 +1,18 @@
 <template>
+  <div>
     <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from '@/page/city/components/Header'
+import CitySearch from '@/page/city/components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
