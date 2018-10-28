784cabf30b548f5400706679e3705c185c2c9bd1
FENGXIUWEI <1428915751@qq.com>
2018-10-28 15:37:02 +0800

    city-header

---
+++ src/page/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+    <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from '@/page/city/components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/page/home/components/Header.vue
+++ src/page/home/components/Header.vue
@@ -7,10 +7,10 @@
       <span class="iconfont">&#xe65a;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
+    <router-link tag="div" to="/city" class="header-right">
       北京
       <span class="iconfont">&#xe64a;</span>
-    </div>
+    </router-link>
   </div>
 </template>
 
